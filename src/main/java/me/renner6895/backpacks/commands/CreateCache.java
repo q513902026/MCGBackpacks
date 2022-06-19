601067312d73f1c2de79f53938d2f108a2fd36b5
xiaoyv404 <xiaoyv_404@outlook.com>
2022-06-19 09:25:02 +0800

    [重构]尝试拆分指令

---
+++ src/main/java/me/renner6895/backpacks/commands/CreateCache.java
@@ -0,0 +1,16 @@
+package me.renner6895.backpacks.commands;
+
+import me.renner6895.backpacks.commands.abstractclass.HopeCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author xiaoyv_404
+ */
+public class CreateCache extends HopeCommand {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        return true;
+    }
+}
