e804e04c2719bdc49a362e6ec246f2e8aa6f110e
xiaoyv404 <xiaoyv_404@outlook.com>
2022-07-01 23:45:47 +0800

    [修复]代码清理

--- src/main/java/me/renner6895/backpacks/commands/CloneCommand.java
+++ src/main/java/me/renner6895/backpacks/commands/CloneCommand.java
@@ -22,11 +22,11 @@ public class CloneCommand extends HopeCommand {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         final Player player = (Player) commandSender;
-        if (this.plugin.itemIsBackpack(player.getInventory().getItemInMainHand())) {
+        if (plugin.itemIsBackpack(player.getInventory().getItemInMainHand())) {
             player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() + 1);
             commandSender.sendMessage(ColorTool.color(plugin.getPrefix() + FormatTool.getFormatText("clone.succuse", "&7The backpack item has been cloned!")));
         } else {
-            commandSender.sendMessage(ColorTool.color(this.plugin.getPrefix() + FormatTool.getFormatText("clone.error2", "&cYou must be holding a backpack for this to work!")));
+            commandSender.sendMessage(ColorTool.color(plugin.getPrefix() + FormatTool.getFormatText("clone.error2", "&cYou must be holding a backpack for this to work!")));
         }
         return true;
     }
